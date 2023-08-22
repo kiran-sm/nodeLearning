760b24539f97910a6f13fccc8292e14ef4174579
Kiran <smkiran.09@gmail.com>
2023-08-22 23:11:38 +0530

    10 completed

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('./util')
 const fs = require('fs') 
+const validator = require('validator')
 
 fs.writeFileSync('notes.txt','This file was created by node js Kiran')
 fs.appendFileSync('notes.txt',' SM')
  console.log('app.js');
+
+ console.log(validator.isEmail('asdasd.com'));
+ console.log(validator.isFloat('ar'));
