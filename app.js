e396eb1cd6afe0ae6cd660697e7a7bb0b87e51ab
Kiran <smkiran.09@gmail.com>
2023-08-22 23:16:30 +0530

    Revert "10 completed"
    This reverts commit 760b24539f97910a6f13fccc8292e14ef4174579.

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 require('./util')
 const fs = require('fs') 
-const validator = require('validator')
 
 fs.writeFileSync('notes.txt','This file was created by node js Kiran')
 fs.appendFileSync('notes.txt',' SM')
  console.log('app.js');
-
- console.log(validator.isEmail('asdasd.com'));
- console.log(validator.isFloat('ar'));
