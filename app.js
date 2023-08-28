1c6406cfbfe012fa6e644c82f585ff672a5595ec
Kiran <smkiran.09@gmail.com>
2023-08-28 23:15:40 +0530

    Section3 Completed

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const chalk = require('chalk');
 require('./util')
 const fs = require('fs') 
 
 fs.writeFileSync('notes.txt','This file was created by node js Kiran')
 fs.appendFileSync('notes.txt',' SM')
- console.log('app.js');
+const getNotes = require('./notes.js') 
+console.log('app.js');
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.bold.green.inverse('Hai'))
